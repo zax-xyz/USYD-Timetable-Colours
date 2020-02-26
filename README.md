4b17581fa645f64de048530faa758c4ed3f4bc8e
Zaxutic <zaxutic@gmail.com>
2020-02-26 14:28:59 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# USYD Timetable Colours
+
+## Example
+![alt text](https://github.com/zaxutic/USYD-Timetable-Colours/raw/master/src/img/timetable.png "Colour coded timetable example")
+
+## Requirements
+Installation of this requires a userscript manager such as [Tampermonkey](https://www.tampermonkey.net/).
+
+Tampermonkey is available for all major browsers including [Firefox](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/) and [Chrome](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en).
+
+## Installation
+After installing Tampermonkey (see above) or another userscript manager, get the script [here](https://raw.githubusercontent.com/zaxutic/USYD-Timetable-Colours/master/index.user.js).
+
+Most userscript managers should recognise the file as a userscript and open the installation page for you, otherwise you will have to manually add it into your manager
---
+++ index.user.js
@@ -0,0 +1,63 @@
+// ==UserScript==
+// @name         USYD Timetable Colours
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Colour code USYD timetables
+// @author       Michael Vo
+// @match        http*://www.timetable.usyd.edu.au/personaltimetable/timetable/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  const classColors = {
+    Lecture: {
+      hue: 0,
+      lightness: 60,
+    },
+
+    Lab: {
+      hue: 240,
+      lightness: 60,
+    },
+
+    Tutorial: {
+      hue: 120,
+      lightness: 40,
+    },
+
+    'Evaluation Session': {
+      hue: 30,
+      lightness: 50,
+    },
+
+    Seminar: {
+      hue: 300,
+      lightness: 65,
+    },
+
+    'Master Class': {
+      hue: 195,
+      lightness: 60,
+    }
+  };
+
+  // Simply get all the classes on the timetable
+  const classes = document.getElementsByClassName('timetable-class');
+
+  for (const elem of classes) {
+    // Get title of class, e.g. 'MATH1902 Lecture', 'MATH1902 Tutorial')
+    const heading = elem.children[0].innerHTML;
+
+    for (const name in classColors) {
+      // Check what type of class we're looking at
+      if (heading.includes(name)) {
+        // If it matches a type in our dictionary, then set the background colour using the dictionary value
+        const params = classColors[name];
+        elem.style.background = `hsla(${params.hue}, 100%, ${params.lightness}%, .35)`;
+        break // No point checking all the other types
+      }
+    }
+  }
+})();
